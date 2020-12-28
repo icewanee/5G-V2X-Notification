ffd54a141afe7cd9f20c116f9b9a2814890765eb
icewanee <iamicewanee@gmail.com>
2020-12-28 22:33:18 +0700

    ..

--- frontend/src/page/Home.js
+++ frontend/src/page/Home.js
@@ -5,37 +5,44 @@ import Video from "../component/Video";
 export default function Home() {
   return (
     <div className="Card ">
+      <br />
       <div className="row">
         <div className="col-md-5">
           <h1 className="card-title" style={{ fontFamily: "Courier New" }}>
             - Welcome to 5G-V2X -
           </h1>
+          <br />
         </div>
         <div className="col-md-7"></div>
-      </div>
-      <div
-        className="col-md-12"
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <div style={{}}>
-          <Video />
+
+        <div
+          className="col-md-12"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <div>
+            <Video />
+          </div>
         </div>
-      </div>
-      <div
-        className="col-md-12"
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <a target="_blank" href="http://videezy.com">
-          Free Broll provided by Videezy!
-        </a>
+        <div
+          className="col-md-12"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <a
+            target="_blank"
+            href="http://videezy.com"
+            style={{ color: "white" }}
+          >
+            Free Broll provided by Videezy!
+          </a>
+        </div>
       </div>
     </div>
   );
