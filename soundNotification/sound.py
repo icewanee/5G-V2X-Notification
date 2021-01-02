fd815783201916ae77dcde9fdc07d4ccd1ec6d14
icewanee <iamicewanee@gmail.com>
2021-01-02 15:45:56 +0700

    init

---
+++ soundNotification/sound.py
@@ -0,0 +1,20 @@
+#need to install GPIO Zero in raspberry pi before using.
+
+from gpiozero import Button
+
+
+
+
+def soundAlert():
+# เวลาเริ่ม
+# เปิดเสียง
+# รอปุ่มกด
+# เวลาปิดเสียง
+
+
+def DrowsinessAlert():
+ # drowsiness use this func to alert driver
+ 
+ #call soundAlert
+ #รับค่าซ้ำตอนแจ้งเตือนอยู่
+
