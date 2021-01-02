25ac15a06bddd4571d2623d5d2a30be3bba36be1
wiwan5 <grace-2540@hotmail.com>
2021-01-02 17:04:51 +0700

    add sample.env

--- frontend/src/component/Map.js
+++ frontend/src/component/Map.js
@@ -6,27 +6,36 @@ import {
   Marker,
   InfoWindow,
 } from "react-google-maps";
+const AccidentPos = (props)=>{
+  return (
+    
+    <Marker
+        position= {props.position}
+        /* icon={{
+          url: "https://www.flaticon.com/svg/static/icons/svg/3338/3338951.svg",
+          scaledSize: new window.google.maps.Size(50, 50),
+        }}*/
+      >
+        {/* <InfoWindow position={props.position}/> */}
 
+      </Marker>
+
+  )
+}
+const data = [{ lat: 13.746791, lng: 100.535458 },{ lat: 13.740522160240175, lng: 100.53447914292413 }]
 function location() {
   return (
     <GoogleMap
       defaultZoom={15}
       defaultCenter={{ lat: 13.746791, lng: 100.535458 }}
     >
-      <Marker
-        position={{ lat: 13.746791, lng: 100.535458 }}
-        /* icon={{
-          url: "https://www.flaticon.com/svg/static/icons/svg/3338/3338951.svg",
-          scaledSize: new window.google.maps.Size(50, 50),
-        }}*/
-      >
-        <InfoWindow position={{ lat: 13.746791, lng: 100.535458 }}>
-          <div>Siam</div>
-        </InfoWindow>
-      </Marker>
-      <Marker
-        position={{ lat: 13.740522160240175, lng: 100.53447914292413 }}
-      ></Marker>
+      {data.map(x =>{
+        return <AccidentPos
+          position = {x}
+        />
+      })}
+      
+       
     </GoogleMap>
   );
 }
@@ -47,3 +56,4 @@ export default function Map() {
     </div>
   );
 }
+
