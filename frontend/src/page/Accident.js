58be92b8879e8fef0df19904ce330180682ca71b
icewanee <iamicewanee@gmail.com>
2021-01-02 17:38:51 +0700

    link page

--- frontend/src/page/Accident.js
+++ frontend/src/page/Accident.js
@@ -1,83 +1,96 @@
-import React from "react";
+import React, { Component } from "react";
 import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
 import Map from "../component/Map";
+import history from "../history";
 
-export default function Accident() {
-  return (
-    <div
-      className="Card "
-      style={{
-        paddingTop: 85,
-        paddingBottom: 85,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#0da8db",
-      }}
-    >
+export class Accident extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
       <div
         className="Card "
         style={{
-          paddingTop: 0,
-          backgroundColor: "#55cbf2",
-          borderRadius: 30,
-          paddingBottom: 25,
+          paddingTop: 85,
+          paddingBottom: 85,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#0da8db",
         }}
       >
-        <div className="row">
-          <div
-            className="col-md-12"
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Map />
-          </div>
-        </div>
-        <div className="row" style={{ paddingTop: 25 }}>
-          <div className="col-md-1"></div>
-          <div className="col-md-10 ">
-            <div className="row">
-              <div className="col-md-10">
-                <div
-                  style={{
-                    border: "thick solid ",
-                    borderRadius: 20,
-                    height: 50,
-                    backgroundColor: "#c1e8f5",
-                  }}
-                >
-                  <h3
-                    className="card-title"
-                    style={{ fontFamily: "Courier New" }}
-                  ></h3>
-                </div>
-              </div>
-              <div className="col-md-2" style={{ display: "flex" }}>
-                <button
-                  type="button"
-                  class="btn "
-                  style={{
-                    paddingLeft: 30,
-                    paddingRight: 30,
-                    borderRadius: 30,
-                    fontSize: 18,
-                    backgroundColor: "#078ab5",
-                    border: " solid black",
-                    color: "white",
-                    marginLeft: "auto",
-                  }}
-                >
-                  Back
-                </button>
-              </div>
+        <div
+          className="Card "
+          style={{
+            paddingTop: 0,
+            backgroundColor: "#55cbf2",
+            borderRadius: 30,
+            paddingBottom: 25,
+          }}
+        >
+          <div className="row">
+            <div
+              className="col-md-12"
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Map />
             </div>
           </div>
-          <div className="col-md-1"></div>
+          <div className="row" style={{ paddingTop: 25 }}>
+            <div className="col-md-1"></div>
+            <div className="col-md-10 ">
+              <div className="row">
+                <div className="col-md-10">
+                  <div
+                    style={{
+                      border: "thick solid ",
+                      borderRadius: 20,
+                      height: 50,
+                      backgroundColor: "#c1e8f5",
+                    }}
+                  >
+                    <h3
+                      className="card-title"
+                      style={{ fontFamily: "Courier New" }}
+                    ></h3>
+                  </div>
+                </div>
+                <div className="col-md-2" style={{ display: "flex" }}>
+                  <button
+                    type="button"
+                    class="btn "
+                    onClick={() => this.onClick("/home")}
+                    style={{
+                      paddingLeft: 30,
+                      paddingRight: 30,
+                      borderRadius: 30,
+                      fontSize: 18,
+                      backgroundColor: "#078ab5",
+                      border: " solid black",
+                      color: "white",
+                      marginLeft: "auto",
+                    }}
+                  >
+                    Back
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-1"></div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+  onClick = async (page) => {
+    console.log("jj");
+    history.push(page);
+  };
 }
+export default Accident;
--- frontend/src/page/Home.js
+++ frontend/src/page/Home.js
@@ -1,86 +1,103 @@
 import React, { Component } from "react";
 import red from "../pictureNvideo/redd.png";
+import history from "../history";
 
-export default function Home() {
-  return (
-    <div
-      className="Card "
-      style={{
-        paddingTop: 85,
-        paddingBottom: 85,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#0da8db",
-      }}
-    >
+export class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
       <div
         className="Card "
         style={{
-          paddingTop: 40,
-          backgroundColor: "#55cbf2",
-          borderRadius: 30,
-          paddingBottom: 40,
+          paddingTop: 85,
+          paddingBottom: 85,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#0da8db",
         }}
       >
-        <div className="row">
-          <div className="col-md-7 ">
-            <h1
-              className="card-title"
-              style={{
-                fontFamily: "Courier New",
-                fontSize: 45,
-                fontWeight: "bold",
-                paddingLeft: 30,
-              }}
-            >
-              Welcome to 5G-V2X
-            </h1>
+        <div
+          className="Card "
+          style={{
+            paddingTop: 40,
+            backgroundColor: "#55cbf2",
+            borderRadius: 30,
+            paddingBottom: 40,
+          }}
+        >
+          <div className="row">
+            <div className="col-md-7 ">
+              <h1
+                className="card-title"
+                style={{
+                  fontFamily: "Courier New",
+                  fontSize: 45,
+                  fontWeight: "bold",
+                  paddingLeft: 30,
+                }}
+              >
+                Welcome to 5G-V2X
+              </h1>
+            </div>
+            <div className="col-md-5 ">
+              <button
+                type="button"
+                class="btn "
+                onClick={() => this.onClick("/accident")}
+                style={{
+                  paddingLeft: 30,
+                  paddingRight: 30,
+                  paddingTop: 15,
+                  paddingBottom: 15,
+                  borderRadius: 50,
+                  fontSize: 20,
+                  backgroundColor: "#078ab5",
+                  border: " solid white",
+                  color: "white",
+                }}
+              >
+                Map
+              </button>
+              <button
+                /*type="button"*/
+                /*class="btn"*/
+                onClick={() => this.onClick("/")}
+                style={{
+                  paddingLeft: 30,
+                  paddingRight: 30,
+                  paddingTop: 15,
+                  paddingBottom: 15,
+                  borderRadius: 50,
+                  fontSize: 20,
+                  backgroundColor: "#078ab5",
+                  border: " solid white",
+                  color: "white",
+                  marginRight: "auto",
+                }}
+              >
+                Logout
+              </button>
+            </div>
           </div>
-          <div className="col-md-5 ">
-            <button
-              type="button"
-              class="btn "
-              style={{
-                paddingLeft: 30,
-                paddingRight: 30,
-                paddingTop: 15,
-                paddingBottom: 15,
-                borderRadius: 50,
-                fontSize: 20,
-                backgroundColor: "#078ab5",
-                border: " solid white",
-                color: "white",
-              }}
+          <div className="row">
+            <div
+              className="col-md-10"
+              style={{ width: "90vw", height: "60vh" }}
             >
-              Map
-            </button>{" "}
-            <button
-              type="button"
-              class="btn "
-              style={{
-                paddingLeft: 30,
-                paddingRight: 30,
-                paddingTop: 15,
-                paddingBottom: 15,
-                borderRadius: 50,
-                fontSize: 20,
-                backgroundColor: "#078ab5",
-                border: " solid white",
-                color: "white",
-                marginRight: "auto",
-              }}
-            >
-              Logout
-            </button>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-10" style={{ width: "90vw", height: "60vh" }}>
-            <img src={red} style={{ width: "87vw", height: "60vh" }} />
+              <img src={red} style={{ width: "87vw", height: "60vh" }} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+  onClick = async (page) => {
+    console.log("jj");
+    history.push(page);
+  };
 }
+export default Home;
