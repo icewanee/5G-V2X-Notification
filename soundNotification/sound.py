badd928f55b2c3bce4a75cea95feddf3694ea9bb
icewanee <iamicewanee@gmail.com>
2021-01-11 22:02:37 +0700

    try import sound :(

--- soundNotification/sound.py
+++ soundNotification/sound.py
@@ -1,15 +1,28 @@
-#need to install GPIO Zero in raspberry pi before using.
+#import multiprocessing
+import time
+
 isAlert = False
 
 def soundAlert():
-    print("hh")
-# เวลาเริ่ม
+    print('sound  here')
+
+
+
+def startAlert():
+    start = time.time()
+    soundAlert()
+    end = time.time()
+    total = start - end
+    print(total)
 # เปิดเสียง
 # รอปุ่มกด
 # เวลาปิดเสียง
 
-
 def notification():
+    global isAlert
     if(not isAlert):
         isAlert = True
-        
+        startAlert()
+
+
+notification()
