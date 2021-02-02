5e2d28dee8b58c8009acaf129853d89fc28bfcf4
wiwan5 <grace-2540@hotmail.com>
2021-02-02 23:24:33 +0700

    add kafka

--- soundNotification/sound.py
+++ soundNotification/sound.py
@@ -3,6 +3,43 @@ import multiprocessing
 import time
 from playsound import playsound
 import keyboard 
+import json
+from kafka import KafkaProducer
+from decouple import config
+from threading import Thread
+
+
+
+KAFKA_BROKER_URL = config('KAFKA_BROKER_IN_CAR_URL')
+TRANSACTIONS_TOPIC_IN_CUSTOMER = config('TRANSACTIONS_TOPIC_IN_CUSTOMER')
+TRANSACTIONS_PER_SECOND = 0.01
+SLEEP_TIME = 1 / TRANSACTIONS_PER_SECOND
+USERNAME = config('USERNAME_IN_CAR')
+PASSWORD = config('PASSWORD_IN_CAR')
+
+class Transaction():
+    def __init__(self):
+        self.producer = KafkaProducer(
+            bootstrap_servers=KAFKA_BROKER_URL,
+            value_serializer=lambda value: json.dumps(value).encode(),
+            api_version=(0, 10, 1),
+            # ver sasl plain
+            # security_protocol='SASL_PLAINTEXT',
+            # sasl_mechanism='PLAIN',
+            # sasl_plain_username=USERNAME,           
+            # sasl_plain_password=PASSWORD,
+            
+        )
+        self.response_time = 0
+
+    def create_transaction_drowsiness(self,t):
+        """Create a fake, randomised transaction."""
+        transaction: dict = {
+            'condition': 'send_dds',
+            'response_time': t,
+        }
+        self.producer.send(TRANSACTIONS_TOPIC_IN_CUSTOMER, value=transaction) 
+        print(transaction)
 
 def sound():
     print("here")
@@ -19,24 +56,29 @@ def soundAlert():
     p.terminate()
 
 
-def startAlert():
+def startAlert(transaction):
     start = time.time()
     soundAlert()
     end = time.time()
     total =  end - start
+    transaction.create_transaction_drowsiness(total)
     print(total)
+
 # เปิดเสียง
 # รอปุ่มกด
 # เวลาปิดเสียง
 
-def notification():
+def notification(transaction):
     global isAlert
     if(not isAlert):
         isAlert = True
-        startAlert()
+        startAlert(transaction)
     return 0
 
 if __name__ == '__main__':
     isAlert = False
     #pygame.init()
-    start = notification()
+    transaction1 = Transaction()
+    while True:
+        start = notification(transaction1)
+        time.sleep(20) # for 
