00c1062e2588c8bc023aeec9fd129a9dbc16269a
icewanee <iamicewanee@gmail.com>
2021-04-03 16:22:47 +0700

    update

--- frontend/src/redesign/Playlist.js
+++ frontend/src/redesign/Playlist.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { Form, Input, Button, Layout, Menu } from "antd";
+import { Form, Input, Button, Layout, Menu, Row, Col } from "antd";
+import { Table, Tag, Space } from "antd";
 
 import "../App.css";
 import history from "../history";
@@ -14,6 +15,48 @@ export class Playlist extends Component {
   render() {
     const { Header, Footer, Sider, Content } = Layout;
     const { SubMenu } = Menu;
+    const columns = [
+      {
+        title: "Song",
+        dataIndex: "Song",
+        key: "Song",
+        render: (text) => <a>{text}</a>,
+      },
+      {
+        title: "Artist",
+        dataIndex: "Artist",
+        key: "Artist",
+        render: (text) => <a>{text}</a>,
+      },
+    ];
+
+    const data = [
+      {
+        key: "1",
+        Song: "Confident",
+        Artist: "Demi Lovato",
+      },
+      {
+        key: "2",
+        Song: "Warm Blood",
+        Artist: "Carly Rae Jepsen",
+      },
+      {
+        key: "3",
+        Song: "Money On My Mind",
+        Artist: "Sam Smith",
+      },
+      {
+        key: "4",
+        Song: "Love Myself",
+        Artist: "Hailee Steinfeld",
+      },
+      {
+        key: "5",
+        Song: "Wake Me Up",
+        Artist: "Avicii",
+      },
+    ];
     return (
       <div
         style={{
@@ -25,8 +68,16 @@ export class Playlist extends Component {
         }}
       >
         <Layout>
-          <Header>
-            <h2 style={{ color: "white" }}>Welcome to 5G-V2X (add username)</h2>
+          <Header
+            style={{
+              // height: "100vh",
+              display: "flex",
+              // justifyContent: "center",
+              alignItems: "center",
+              // backgroundColor: "#c6d5ad",
+            }}
+          >
+            <h3 style={{ color: "white" }}>Welcome to 5G-V2X (add username)</h3>
           </Header>
           <Layout
             className="site-layout-background"
@@ -34,11 +85,46 @@ export class Playlist extends Component {
           >
             <Content
               style={{
-                height: "100vh",
+                height: "100%",
                 backgroundColor: "#c6d5ad",
               }}
             >
-              Content
+              <Row>
+                <Col
+                  span={24}
+                  style={{
+                    // height: "100vh",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <br /> <br /> <br />
+                  <br />
+                  <h4
+                    style={{
+                      fontWeight: "bold",
+                    }}
+                  >
+                    Music playlist
+                  </h4>
+                </Col>
+                <Col
+                  span={24}
+                  style={{
+                    // height: "100vh",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <Table
+                    style={{ width: "50vw" }}
+                    columns={columns}
+                    dataSource={data}
+                  />
+                </Col>
+              </Row>
             </Content>
             <Sider
               className="site-layout-background"
@@ -48,6 +134,7 @@ export class Playlist extends Component {
                 //   justifyContent: "center",
                 //   alignItems: "center",
                 backgroundColor: "white",
+                height: "100%",
               }}
             >
               <Menu
@@ -60,7 +147,7 @@ export class Playlist extends Component {
                 <Menu.Item key="2">SOS</Menu.Item>
                 <Menu.Item key="3">Playlist</Menu.Item>
                 <Menu.Item key="4">Logout</Menu.Item>
-                <Footer>Footer</Footer>
+                {/* <Footer>Footer</Footer> */}
               </Menu>
             </Sider>
           </Layout>
