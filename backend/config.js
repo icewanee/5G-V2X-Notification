a6eab4be0591e84f214769d9b8582adf7be7918a
Wiwan5 <grace-2540@hotmail.com>
2021-04-07 23:07:30 +0700

    edit congif file

--- backend/config.js
+++ backend/config.js
@@ -3,8 +3,6 @@ require('dotenv').config();
 const config = {
   KafkaHostInCar:process.env.KAFKA_HOST_CAR,
   KafkaHost:process.env.KAFKA_HOST,
-  KafkaTopic: process.env.KAFKA_TOPIC,
-  KafkaTopicInCar: process.env.TRANSACTIONS_TOPIC_IN_CAR,
   CloundSever: process.env.API_TO_CLOUND_SERVER,
   TimeDisappearAcs: 3600,
   KafkaUsernameInCar: process.env.USERNAME_KAFKA_IN_CAR,
@@ -15,7 +13,6 @@ const config = {
   KafkaDdsTopic: process.env.KAFKA_DIC_TOPIC,
   KafkaActTopic: process.env.KAFKA_ACD_TOPIC,
   KafkaAICTopic: process.env.KAFKA_AIC_TOPIC,
-  KafkaMusicTopic: process.env.KAFKA_MUSIC_TOPIC,
   KafkaUsernameTopic: process.env.KAFKA_USERNAME_TOPIC,
 };
 
